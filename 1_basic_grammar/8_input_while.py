6ea63f1f26494e321bb99d20d8703616ebc7a8e9
Vicene <vicenelwj@gmail.com>
2023-01-06 22:41:19 +0800

    add:input while

---
+++ 1_basic_grammar/8_input_while.py
@@ -0,0 +1,42 @@
+"""
+    input
+"""
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
+
+
+"""
+    while
+"""
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
+
