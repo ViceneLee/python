bf95b269efc0af92c6fb57f4f06a89449dc2b2b7
Vicene <vicenelwj@gmail.com>
2023-01-07 17:55:32 +0800

    add:class

---
+++ 2_def_function/3_class.py
@@ -0,0 +1,17 @@
+import dog
+from dog import Dog
+from Car import Car, ElectricCar
+
+#from Car import Car as C, ElectricCar as EC
+
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+my_beetle = Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricCar('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
